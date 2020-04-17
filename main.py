1ed2a54650b2dd8bb9995c33fc63d171e2e46f2c
Suhas Arun <suhas.arun142@gmail.com>
2020-04-17 13:49:52 +0100

    Added executable

--- main.py
+++ main.py
@@ -6,25 +6,30 @@ import Board
 import Player
 import Game
 
-window = tkinter.Tk()
-window.title("Monopoly")
-WIDTH = 1280
-HEIGHT = 675
-# sets the window to 1280x675
-window.geometry("{}x{}".format(str(WIDTH), str(HEIGHT)))
+def main():
+    window = tkinter.Tk()
+    window.title("Monopoly")
+    WIDTH = 1280
+    HEIGHT = 675
+    # sets the window to 1280x675
+    window.geometry("{}x{}".format(str(WIDTH), str(HEIGHT)))
 
-canvas = tkinter.Canvas(window, width=WIDTH, height=HEIGHT, bg="#ccffcc")
+    canvas = tkinter.Canvas(window, width=WIDTH, height=HEIGHT, bg="#ccffcc")
 
-places = Place.places
-properties = Place.properties
+    places = Place.places
+    properties = Place.properties
 
-board = Board.Board(window, canvas, places, properties)
+    board = Board.Board(window, canvas, places, properties)
 
-player1 = Player.Player(1, "red")
-player2 = Player.Player(2, "blue")
+    player1 = Player.Player(1, "red")
+    player2 = Player.Player(2, "blue")
 
-Monopoly = Game.Game(window, board, player1, player2)
-Monopoly.setupGame()
-board.roll_button.config(state="normal")
+    Monopoly = Game.Game(window, board, player1, player2)
+    Monopoly.setupGame()
+    board.roll_button.config(state="normal")
 
-window.mainloop()
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
